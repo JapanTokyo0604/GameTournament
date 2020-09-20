cd44b30fb4104c7f51aa2508727dbe4bda3a43d8
indianelephant <elephantindian0117@gmail.com>
2020-09-20 13:46:35 +0900

    initialize commit

---
+++ GameTournament/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GameTournament
+//
+//  Created by I.T on 2020/09/20.
+//  Copyright © 2020 I.T. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
